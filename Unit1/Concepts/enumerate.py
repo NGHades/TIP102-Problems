d4602177fb63d5954121154f11f42c78971bbb05
NGHades <146700140+NGHades@users.noreply.github.com>
2025-09-18 15:48:16 -0700

    Added a concepts folder for concepts seen in Units

---
+++ Unit1/Concepts/enumerate.py
@@ -0,0 +1,8 @@
+my_string = "murder on da beat"
+for index, char in enumerate(my_string):
+    print(index, char)
+
+# For a dictionary
+my_list = ["red", "blue", "green"]
+new_dict = dict(enumerate(my_list))
+print(new_dict)
